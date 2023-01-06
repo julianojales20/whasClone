29375bf13e01bd45453fe06812864385721df24d
Juliano Alonso <julianojales20@gmail.com>
2023-01-06 17:23:52 -0300

    mensagem

---
+++ src/components/ChatIntro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ChatIntro.css';
+
+export default () => {
+    return (
+        <div className="chatIntro">
+            <img src="https://whatsapp-clone-web.netlify.app/static/media/intro-connection-light.5690d473.jpg" alt="Imagem" />
+            <h1>WhatsApp para Chats Interno</h1>
+            <h2>Mantenha suas conversas sem a necessidade de uso de uma conta no WhatsApp.<br/>
+            Não há necessidade de estar com o telefone conectado na Web.</h2>
+        </div>
+    );
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyARDBM7RO6Ua5sR8eA71zi7Q5SCZcjZupE",
+    authDomain: "whatsappclone-41339.firebaseapp.com",
+    projectId: "whatsappclone-41339",
+    storageBucket: "whatsappclone-41339.appspot.com",
+    messagingSenderId: "237648796617",
+    appId: "1:237648796617:web:1ffbbf3679359576fea618"
+  };
